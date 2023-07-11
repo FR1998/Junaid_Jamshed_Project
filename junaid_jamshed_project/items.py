6c633b44b84234781c2edf402073f49ad7fabac6
FR1998 <iamfaizan199@gmail.com>
2023-07-11 12:42:17 +0500

    done

---
+++ junaid_jamshed_project/items.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class Clothing_Item(scrapy.Item):
+    product_name = scrapy.Field()
+    product_price = scrapy.Field()
+    product_images = scrapy.Field()
+    product_info = scrapy.Field()
+    product_code = scrapy.Field()
+    values = scrapy.Field()
+    
